47ea748637bef894beea5ef759fab6468505fefc
ZM284 <zorin.mikhail.84284@gmail.com>
2023-04-17 18:38:55 +0300

    2023-04-17(1)  18-38 -- Homework 04 finished

--- Hillel_PythonPro/04/helpers.py
+++ Hillel_PythonPro/04/helpers.py
@@ -22,22 +22,29 @@ class Column:
         self.alignment = None
         if self.header is not None and len(self.header) > 0:
             match self.header[0]:
-                case '<':
+                case "<":
                     self.alignment = str.ljust
-                case '^':
+                case "^":
                     self.alignment = str.center
-                case '>':
+                case ">":
                     self.alignment = str.rjust
         if self.alignment is None:
             self.alignment = str.ljust
         else:
             self.header = self.header[1:]
-        self.values = [column_info.property_as_string_selector(_item) for _item in items]
-        self.width = max([strlen(_value) for _value in self.values] + [len(self.header)])
-        self.values = [align(_value, self.width, self.alignment) for _value in self.values]
+        self.values = [
+            column_info.property_as_string_selector(_item) for _item in items
+        ]
+        self.width = max(
+            [strlen(_value) for _value in self.values] + [len(self.header)]
+        )
+        self.values = [
+            align(_value, self.width, self.alignment) for _value in self.values
+        ]
         self.header = f" {self.header} ".center(self.width + 2, "─")
         self.footer = "─" * (self.width + 2)
-        # -> 
+        # ->
+
 
 class StrHelper:
     @staticmethod
@@ -48,7 +55,9 @@ class StrHelper:
         _columns = [Column(_column_info, items) for _column_info in columns_info]
         _str += f"\n╭{'┬'.join([f'{_column.header}' for _column in _columns])}╮"
         for _i in range(len(items)):
-            _str += f"\n│{'│'.join([f' {_column.values[_i]} ' for _column in _columns])}│"
+            _str += (
+                f"\n│{'│'.join([f' {_column.values[_i]} ' for _column in _columns])}│"
+            )
         _str += f"\n╰{'┴'.join([f'{_column.footer}' for _column in _columns])}╯"
         return _str
 
@@ -56,7 +65,7 @@ class StrHelper:
     def get_only_digits(value: str) -> str:
         _str = ""
         if value is not None and len(value) > 0:
-            _str = _str.join(re.findall(r'\d+', value))
+            _str = _str.join(re.findall(r"\d+", value))
         return _str
 
     @staticmethod
@@ -64,12 +73,13 @@ class StrHelper:
         if substring is None or len(substring) == 0:
             return True
         _str = string.lower()
-        _sub_str = substring.strip('*').lower()
-        if substring.startswith('*') and not substring.endswith('*'):
+        _sub_str = substring.strip("*").lower()
+        if substring.startswith("*") and not substring.endswith("*"):
             return _str.endswith(_sub_str)
-        if not substring.startswith('*') and substring.endswith('*'):
+        if not substring.startswith("*") and substring.endswith("*"):
             return _str.startswith(_sub_str)
         return _sub_str in _str
+
 
 class AsStr:
     @staticmethod
@@ -100,14 +110,20 @@ class UsrInput:
             try:
                 _user_input = int(input(f"{input_str} > "))
                 if int_constraints is not None and _user_input not in int_constraints:
-                    print(f"\33[41mПОМИЛКА ВВОДУ ДАННИХ.\33[0m оберіть з поміж \33[33mДОСТУПНИХ\33[0m числових значень!")
+                    print(
+                        "\33[41mПОМИЛКА ВВОДУ ДАННИХ.\33[0m оберіть з поміж \33[33mДОСТУПНИХ\33[0m числових значень!"
+                    )
                     _user_input = None
             except ValueError:
-                print(f"\33[41mПОМИЛКА ВВВОДУ ДАННИХ.\33[0m Оберіть \33[33mЧИСЛОВЕ\33[0m значення!")
+                print(
+                    "\33[41mПОМИЛКА ВВВОДУ ДАННИХ.\33[0m Оберіть \33[33mЧИСЛОВЕ\33[0m значення!"
+                )
         return _user_input
 
     @staticmethod
-    def get_str(input_str: str, min_len: int = 0, str_constraints: list[str] = []) -> str:
+    def get_str(
+        input_str: str, min_len: int = 0, str_constraints: list[str] = []
+    ) -> str:
         """Отримати введену з консолі користувачем стрічку
         :param input_str: Текст запрошення вводу даних для користувача
         :param min_len: [Опціонально] Мінімальна довжина рядка, який повинен ввести користувач
@@ -117,11 +133,23 @@ class UsrInput:
         _user_input: str | None = None
         while _user_input is None:
             _user_input = input(f"{input_str} > ")
-            if min_len is not None and _user_input is not None and len(_user_input) < min_len:
-                print(f"\33[41mПОМИЛКА ВВОДУ ДАННИХ.\33[0m Ведіть рядок не коротше ніж {min_len} символи")
+            if (
+                min_len is not None
+                and _user_input is not None
+                and len(_user_input) < min_len
+            ):
+                print(
+                    f"\33[41mПОМИЛКА ВВОДУ ДАННИХ.\33[0m Ведіть рядок не коротше ніж {min_len} символи"
+                )
                 _user_input = None
-            if str_constraints is not None and len(str_constraints) > 0 and _user_input not in str_constraints:
-                print(f"\33[41mПОМИЛКА ВВОДУ ДАННИХ.\33[0m Ведіть рядок з поміж доступних варіантів ({str_constraints})")
+            if (
+                str_constraints is not None
+                and len(str_constraints) > 0
+                and _user_input not in str_constraints
+            ):
+                print(
+                    f"\33[41mПОМИЛКА ВВОДУ ДАННИХ.\33[0m Ведіть рядок з поміж доступних варіантів ({str_constraints})"
+                )
                 _user_input = None
         return _user_input
 
@@ -142,23 +170,23 @@ class UsrInput:
                     else:
                         raise ValueError("Date is None")
 
-                _temp_res: datetime = datetime.strptime(_usr_input, '%Y-%m-%d')
+                _temp_res: datetime = datetime.strptime(_usr_input, "%Y-%m-%d")
                 if _temp_res is None:
-                    print(f"\33[41mПОМИЛКА ВВОДУ ДАННИХ.\33[0m значення обов'язкове!")
+                    print("\33[41mПОМИЛКА ВВОДУ ДАННИХ.\33[0m значення обов'язкове!")
                     _result = None
                 else:
                     _result = date(_temp_res.year, _temp_res.month, _temp_res.day)
             except ValueError:
-                print(f"\33[41mПОМИЛКА ВВВОДУ ДАННИХ.\33[0m Введіть \33[33mДАТУ\33[0m в форматі yyyy-MM-dd")
+                print(
+                    "\33[41mПОМИЛКА ВВВОДУ ДАННИХ.\33[0m Введіть \33[33mДАТУ\33[0m в форматі yyyy-MM-dd"
+                )
                 _result = None
         return _result
-
-
-
 
 
 def main():
     ...
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
--- Hillel_PythonPro/04/homework04.py
+++ Hillel_PythonPro/04/homework04.py
@@ -2,69 +2,72 @@
 from typing import Any
 from helpers import ColumnInfo, StrHelper, UsrInput
 
-NAME = 'name'
-AGE = 'age'
-NUMB = 'number'
+NAME = "name"
+AGE = "age"
+NUMB = "number"
 
 
-#def get_name(player: dict, default: str = ""):
+# def get_name(player: dict, default: str = ""):
 #    return player[NAME] if NAME in player else default
 
 
-#def get_age(player: dict, default: int | None = None):
+# def get_age(player: dict, default: int | None = None):
 #    return player[AGE] if AGE in player else default
 
 
-#def get_numb(player: dict, default: int | None = None):
+# def get_numb(player: dict, default: int | None = None):
 #    return player[NUMB] if NUMB in player else default
 
 
 def as_view_str(title: str, records: list[dict]) -> str:
-    _str_table = StrHelper.as_table(title, records, [
-        ColumnInfo('>Number', lambda e: str(e['number'])),
-        ColumnInfo('Name', lambda e: e['name']),
-        ColumnInfo('>Age', lambda e: str(e['age']))
-    ])
+    _str_table = StrHelper.as_table(
+        title,
+        records,
+        [
+            ColumnInfo(">Number", lambda e: str(e["number"])),
+            ColumnInfo("Name", lambda e: e["name"]),
+            ColumnInfo(">Age", lambda e: str(e["age"])),
+        ],
+    )
     return _str_table
 
 
 def players_repr(players: list[dict], verbose: bool) -> None:
-    table_header = 'List of team members' if verbose else "Players"
+    table_header = "List of team members" if verbose else "Players"
     print(as_view_str(table_header, players))
 
 
 def players_add(players: list[dict], player: dict) -> list[dict]:
     if players is None:
-        raise Exception('players is None')
+        raise Exception("players is None")
     if player is None:
-        raise Exception('Player is None')
+        raise Exception("Player is None")
     players.append(player)
     return players
 
 
 def players_index(players: list[dict], field: str, value: Any) -> int:
     if players is None:
-        raise Exception('players is None')
-    if value == None:
-        raise Exception('value is None')
+        raise Exception("players is None")
+    if value is None:
+        raise Exception("value is None")
     _indexes = []
     _index = 0
-    _value = None;
+    _value = None
     while _index < len(players):
         _player = players[_index]
         if field.lower() == NAME:
-            _value = (_player[NAME] if NAME in _player else None)
+            _value = _player[NAME] if NAME in _player else None
         elif field.lower() == AGE:
-            _value = (str(_player[AGE]) if AGE in _player else None)
+            _value = str(_player[AGE]) if AGE in _player else None
         elif field.lower() == NUMBER:
-            _value = (str(_player[NUMBER]) if NUMBER in _player else None)
+            _value = str(_player[NUMBER]) if NUMBER in _player else None
         else:
             raise Exception(f'Field "{field}" is unknown')
         if _value.lower() == value.lower():
             _indexes.append(_index)
         _index += 1
     return _indexes
-
 
 
 def players_del(players: list[dict], name: str) -> list[dict]:
@@ -102,33 +105,35 @@ def main():
     available_fields = [NUMB, NAME, AGE]
 
     while True:
-        match UsrInput.get_str(f'\n\nEnter your choice {options}', None, options):
-            case 'repr':
+        match UsrInput.get_str(f"\n\nEnter your choice {options}", None, options):
+            case "repr":
                 players_repr(team, False)
-            case 'add':
-                print('New player')
-                _number = UsrInput.get_number(' - number')
-                _name = UsrInput.get_str(' - name  ', 3)
-                _age = UsrInput.get_number(' - age   ')
-                _new_player = {NAME: _name, AGE: _age, NUMB: _number}                
+            case "add":
+                print("New player")
+                _number = UsrInput.get_number(" - number")
+                _name = UsrInput.get_str(" - name  ", 3)
+                _age = UsrInput.get_number(" - age   ")
+                _new_player = {NAME: _name, AGE: _age, NUMB: _number}
                 players_repr(players_add(team, _new_player), True)
-            case 'del':
-                _player_name = UsrInput.get_str('Player name', 1)
+            case "del":
+                _player_name = UsrInput.get_str("Player name", 1)
                 players_repr(players_del(team, _player_name), False)
-            case 'find':
-                _field_name = UsrInput.get_str(f'Field name (from {available_fields})', None, available_fields)
-                _value = UsrInput.get_str('Field value')
+            case "find":
+                _field_name = UsrInput.get_str(
+                    f"Field name (from {available_fields})", None, available_fields
+                )
+                _value = UsrInput.get_str("Field value")
                 _founded_players = players_find(team, _field_name, _value)
                 players_repr(_founded_players, True)
-            case 'get':
-                _player_name = UsrInput.get_str('Player name', 1)
+            case "get":
+                _player_name = UsrInput.get_str("Player name", 1)
                 _founded_player = players_get_by_name(team, _player_name)
                 if _founded_player is None:
-                    print('No one player found')
+                    print("No one player found")
                 else:
-                    players_repr([_founded_player], True)                
-            case 'exit':
-                print('Program exit');
+                    players_repr([_founded_player], True)
+            case "exit":
+                print("Program exit")
                 break
 
 
