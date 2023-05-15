d2d915bba723c7ca6c4f298334fa7b88963eab5a
ZM284 <zorin.mikhail.84284@gmail.com>
2023-05-16 01:25:31 +0300

    :construction: In progress

--- Hillel_PythonPro/10/homework10.py
+++ Hillel_PythonPro/10/homework10.py
@@ -1,3 +1,57 @@
-# Homework 10 started
+from pprint import pprint as print
+import requests
+import asyncio
 
-# ...
+
+class ExchangeRates:
+    _instance = None
+    _initialized = False
+
+    def __new__(cls, *args, **kwargs):
+        if cls._instance is None:
+            cls._instance = super().__new__(cls)
+
+        return cls._instance
+
+    def __init__(self) -> None:
+        if ExchangeRates._initialized:
+            return
+
+        self.data: dict = self._fetch_from_api()
+
+        ExchangeRates._initialized = True
+
+    @staticmethod
+    def _fetch_from_api() -> dict:
+        url = "https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=USD&to_currency=UAH&apikey=PASTE_YOUR_API_KEY"
+        response = requests.get(url)
+        return response.json()
+
+    # @staticmethod
+    # def get_rate(source: str, target: str) -> float:
+    #     url = f"https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency={source}&to_currency={target}&apikey=PASTE_YOUR_API_KEY"
+    #     response = requests.get(url)
+    #     return response.json()["5. Exchange rate"]
+
+
+er = ExchangeRates()
+# er = object.__new__(ExchangeRates)
+# er <= ExchangeRages.__init__() <= ExchangeRates.__new__()
+
+
+print(er.data)
+# er2 = ExchangeRates()
+# er2 = ExchangeRates()
+# er2 = ExchangeRates()
+# er2 = ExchangeRates()
+# er2 = ExchangeRates()
+# print(er2.data)
+
+async def main():
+    async with asyncio.TaskGroup() as tg:
+        task1 = tg.create_task(...)
+        task2 = tg.create_task(...)
+    print("all tasks have completed now.")
+
+if __name__ == '__main__':
+    asyncio.run(main())
